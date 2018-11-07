3605193424e2be920a043e98eb0dbdef550f8663
Vuppala Sahithya <35240462+VuppalaSahithya@users.noreply.github.com>
2018-11-07 01:54:58 -0800

    Update 5.py

--- 5.py
+++ 5.py
@@ -1,11 +1,16 @@
 # Unix / Linux system commands with basic options / flags well known
-#Linux comands
-cd - change directory
+
 pwd - To view present working directory
 mkdir - to make directory
+cd - change directory
+cd ../ - to go back one level directory
 rm - removes file from directory
+rm -r - removes the directory
 rmdir - remove directory
 ls -lists all contents of directory
+ls -a - lists the hidden files
+cat - used to view contents of a file 
+touch - used to create a file
 kill - kill process running with pid
 chmod -  changes/updates the user and group ownership of a file/directory
 clear - clears the terminal screen
@@ -14,3 +19,6 @@ echo - prints a text of line provided to it
 exit - used to exit a shell
 head - show first lines of the specified file
 history - shows previously used commands or to get info about command executed
+vi - opens a vi editor 
+grep - search a file for particular pattern of characters
+
